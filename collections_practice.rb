40f56e1d091e31af648a50a008cd4f387ba3727d
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-06-14 15:01:27 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,4 +10,6 @@ def sort_array_char_count(strings)
   strings.sort {|left,right| left.length <=> right.length}
 end 
 
-def 
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+end
