d8a09826d80276a30817562e2046d414a8c23ee0
Trejon Stallsworth <trejonstallsworth@gmail.com>
2019-06-14 14:59:16 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -5,3 +5,7 @@ end
 def sort_array_desc(array)
   array.sort { |first_array, second_array| second_array <=> first_array}
 end
+
+def sort_array_char_count(strings)
+  array.sort {|left,right| left.length <=> right.length}
+end 
